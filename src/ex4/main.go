a38adb0178b472a8d9e42713ff6f51ffbcfd5295
tran1619 <tran1619@gmail.com>
2016-10-02 22:05:16 -0400

    added padded zero(s) for matrix

--- src/ex4/main.go
+++ src/ex4/main.go
@@ -10,7 +10,7 @@ func main() {
 		{5, 5, 7, 8},
 		{9, 11, 11},
 		{12, 13, 15, 14},
-		{16, 17, 18, 19, 20, 32}}
+		{16, 17, 18, 19, 20}}
 
 	slice := UniqueInts(irregularMatrix)
 	fmt.Printf("1x%d: %v\n", len(slice), slice)
@@ -18,8 +18,11 @@ func main() {
 	slice = Flatten(irregularMatrix)
 	fmt.Printf("1x%d: %v\n", len(slice), slice)
 
-	matrix := Make2D(slice, 3)
-	Print2D(matrix)
+	// Printing 2D matrix
+	fmt.Printf("3x%d: %v\n", ComputeRow(slice, 3), Make2D(slice, 3))
+	fmt.Printf("4x%d: %v\n", ComputeRow(slice, 4), Make2D(slice, 4))
+	fmt.Printf("5x%d: %v\n", ComputeRow(slice, 5), Make2D(slice, 5))
+	fmt.Printf("6x%d: %v\n", ComputeRow(slice, 6), Make2D(slice, 6))
 
 }
 
@@ -30,7 +33,7 @@ func Print2D(matrix [][]int) {
 func ComputeRow(matrix []int, column int) int {
 	row := len(matrix) / column
 	col := len(matrix) % column
-	fmt.Println("row -> col\n", row, col)
+	// fmt.Println("row -> col\n", row, col)
 	if col > 0 {
 		row++
 	}
@@ -39,18 +42,18 @@ func ComputeRow(matrix []int, column int) int {
 
 func Make2D(matrix []int, column int) [][]int {
 	mMatrix := make([][]int, ComputeRow(matrix, column))
-	j := column
-	k := 0
+	//	j := column
+	//	k := 0
 
-	for _, val := range matrix {
-		if j > 0 {
-			mMatrix[k] = append(mMatrix[k], val)
-			j--
-		} else {
-			k++
-			mMatrix[k] = append(mMatrix[k], val)
-			j = column - 1
+	for i, val := range matrix {
+		row := i / column
+		col := i % column
+		//fmt.Printf("i:%d -> row:%d => col:%d\n", i, row, col)
+		// we allocate when column is starting (i.e. col is a mod of column)
+		if col == 0 {
+			mMatrix[row] = make([]int, column)
 		}
+		mMatrix[row][col] = val
 	}
 	return mMatrix
 }
